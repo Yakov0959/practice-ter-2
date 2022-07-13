99a746277be129d5a072ddb6dbfdcb4db2197878
Yakov Korotia <yakov0959@gmail.com>
2022-07-13 16:25:50 +0300

    fmt

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "AK" {
-  type = string
+  type        = string
   description = "access key ID"
 }
 
 variable "SK" {
-  type = string
-description = "secret access key"
+  type        = string
+  description = "secret access key"
 }
 
 variable "region" {
-  type    = string
- default = "eu-west-2"
+  type        = string
+  default     = "eu-west-2"
   description = "Regions"
 }
 
 variable "availability_zones" {
   type        = string
-  default = "eu-west-2c"
+  default     = "eu-west-2c"
   description = "Availability Zones"
 }
